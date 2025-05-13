c9accf41d8e64535b4229f63a9e4201cc74556e7
Jovenjr <samueljuniormartinez@gmail.com>
2025-05-13 15:50:48 -0400

    ActualizaciÃ³n de imÃ¡genes de servicios y hero

--- git-push.ps1
+++ git-push.ps1
@@ -3,4 +3,4 @@ git init
 git add .
 git commit -m "Actualización de imágenes de servicios y hero"
 git remote add origin https://github.com/Jovenjr/profepina.git
-git push -u origin master
+git push -u origin main
